8f172b6889c102003d66c343a11bdc2b0dc534ee
Boris Lin <bolin@zetaglobal.com>
2020-09-04 09:18:32 -0700

    changes to make gulp work

--- modules/zetaBidAdapter.js
+++ modules/zetaBidAdapter.js
@@ -1,124 +1,111 @@
-import * as utils from 'src/utils';
-import {config} from 'src/config';
-import {registerBidder} from 'src/adapters/bidderFactory';
+import * as utils from '../src/utils.js';
+// import { config } from '../src/config.js';
+import { registerBidder } from '../src/adapters/bidderFactory.js';
 const BIDDER_CODE = 'Zeta Global';
 const ENDPOINT_URL = 'https://prebid-us-east.rfihub.com:2080/prebid';
 
 export const spec = {
-    code: BIDDER_CODE,
+  code: BIDDER_CODE,
 
-    /**
+  /**
      * Determines whether or not the given bid request is valid.
      *
      * @param {BidRequest} bid The bid params to validate.
      * @return boolean True if this is a valid bid, and false otherwise.
      */
-    isBidRequestValid: function(bid) {
-        return !!(bid.params.placementId || (bid.params.member && bid.params.invCode));
-    },
+  isBidRequestValid: function(bid) {
+    return !!(bid.params.placementId || (bid.params.member && bid.params.invCode));
+  },
 
-    /**
+  /**
      * Make a server request from the list of BidRequests.
      *
      * @param {validBidRequests[]} - an array of bidRequest objects
      * @param {bidderRequest} - master bidRequest object
      * @return ServerRequest Info describing the request to the server.
      */
-    buildRequests: function(validBidRequests, bidderRequest) {
-        let payload = {
-            bidderRequest: bidderRequest,
-            validBidRequests: validBidRequests
-        }
-        return {
-            method: 'POST',
-            url: ENDPOINT_URL,
-            data: JSON.stringify(payload),
-        };
-    },
+  buildRequests: function(validBidRequests, bidderRequest) {
+    let payload = {
+      bidderRequest: bidderRequest,
+      validBidRequests: validBidRequests
+    };
+    utils.logMessage('paylod generated');
+    return {
+      method: 'POST',
+      url: ENDPOINT_URL,
+      data: JSON.stringify(payload),
+    };
+  },
 
-    /**
+  /**
      * Unpack the response from the server into a list of bids.
      *
      * @param {ServerResponse} serverResponse A successful response from the server.
      * @return {Bid[]} An array of bids which were nested inside the server.
      */
-    interpretResponse: function(serverResponse, bidRequest) {
-        // const serverBody  = serverResponse.body;
-        // const headerValue = serverResponse.headers.get('some-response-header');
-        const bidResponses = [];
-        const bidResponse = {
-            requestId: bidRequest.bidId,
-            cpm: CPM,
-            width: WIDTH,
-            height: HEIGHT,
-            creativeId: CREATIVE_ID,
-            dealId: DEAL_ID,
-            currency: CURRENCY,
-            netRevenue: true,
-            ttl: TIME_TO_LIVE,
-            referrer: REFERER,
-            ad: CREATIVE_BODY
-        };
-        bidResponses.push(bidResponse);
-    };
+  interpretResponse: function(serverResponse, bidRequest) {
+    // const serverBody  = serverResponse.body;
+    // const headerValue = serverResponse.headers.get('some-response-header');
+    const bidResponses = [];
     return bidResponses;
+  },
 
-    /**
+  /**
      * Register the user sync pixels which should be dropped after the auction.
      *
      * @param {SyncOptions} syncOptions Which user syncs are allowed?
      * @param {ServerResponse[]} serverResponses List of server's responses.
      * @return {UserSync[]} The user syncs which should be dropped.
      */
-    getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {
-       const syncs = []
+  getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {
+    const syncs = []
 
-       var gdpr_params;
-       if (typeof gdprConsent.gdprApplies === 'boolean') {
-           gdpr_params = `gdpr=${Number(gdprConsent.gdprApplies)}&gdpr_consent=${gdprConsent.consentString}`;
-       } else {
-           gdpr_params = `gdpr_consent=${gdprConsent.consentString}`;
-       }
-
-        if (syncOptions.iframeEnabled) {
-            syncs.push({
-                type: 'iframe',
-                //TODO: Change this sync URL to the Zeta URL
-                url: '//acdn.adnxs.com/ib/static/usersync/v3/async_usersync.html?' + gdpr_params
-            });
-        }
-        if (syncOptions.pixelEnabled && serverResponses.length > 0) {
-            syncs.push({
-                type: 'image',
-                url: serverResponses[0].body.userSync.url + gdpr_params
-            });
-        }
-        return syncs;
+    var gdprParams;
+    if (typeof gdprConsent.gdprApplies === 'boolean') {
+      gdprParams = `gdpr=${Number(gdprConsent.gdprApplies)}&gdpr_consent=${gdprConsent.consentString}`;
+    } else {
+      gdprParams = `gdpr_consent=${gdprConsent.consentString}`;
     }
 
-    /**
+    if (syncOptions.iframeEnabled) {
+      syncs.push({
+        type: 'iframe',
+        // TODO: Change this sync URL to the Zeta URL
+        url: '//acdn.adnxs.com/ib/static/usersync/v3/async_usersync.html?' + gdprParams
+      });
+    }
+    if (syncOptions.pixelEnabled && serverResponses.length > 0) {
+      syncs.push({
+        type: 'image',
+        url: serverResponses[0].body.userSync.url + gdprParams
+      });
+    }
+    return syncs;
+  },
+
+  /**
      * Register bidder specific code, which will execute if bidder timed out after an auction
      * @param {data} Containing timeout specific data
      */
-    onTimeout: function(data) {
-        // Bidder specifc code
-    }
+  onTimeout: function(data) {
+    // Bidder specifc code
+  },
 
-    /**
+  /**
      * Register bidder specific code, which will execute if a bid from this bidder won the auction
      * @param {Bid} The bid that won the auction
      */
-    onBidWon: function(bid) {
-        // Bidder specific code
-    }
+  onBidWon: function(bid) {
+    // Bidder specific code
+  },
 
-    /**
+  /**
      * Register bidder specific code, which will execute when the adserver targeting has been set for a bid from this bidder
      * @param {Bid} The bid of which the targeting has been set
      */
-    onSetTargeting: function(bid) {
-        // Bidder specific code
-    }
+  onSetTargeting: function(bid) {
+    // Bidder specific code
+  }
 }
 
 registerBidder(spec);
